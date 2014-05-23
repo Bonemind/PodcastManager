dc3f634acc9f117b6b08591e39cba3e8408849dc
Bonemind <subhime@gmail.com>
2014-05-23 20:13:41 +0200

    Added gruntfile, basic bootstrap scaffolding and files

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    concat: {
+      js: {
+        src: [
+          "models/**/*.js",
+          "collections/**/*.js",
+          "controllers/**/*.js",
+          "views/**/*.js",
+        ],
+        dest: "build/app.js"
+      },
+      html: {
+	src: [
+	  "main.html",
+	  "templates/**/*.html",
+	  "foot.html",
+        ],
+        dest: "index.html",
+      },
+    },
+    watch: {
+	  scripts: {
+	    files: ['**/*.js', '**/*.html','!build/*.js', '!index.html'],
+	    tasks: ['concat'],
+	  },
+	},
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.registerTask("default", ["concat"]);
+}
